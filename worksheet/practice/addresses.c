2be65be8e4a08567a8e810377961374ed6bfb8fc
lukakamidzorac <sjvj0907@leeds.ac.uk>
2026-02-16 13:33:41 +0000

    end of addresses.c

--- worksheet/practice/addresses.c
+++ worksheet/practice/addresses.c
@@ -13,6 +13,6 @@ int main( void ) {
     printf(" %p , %p\n",&k1,&k2);  // we print the memory addresses of both
 
     // what can you deduce about the memory usage and memory layout from inspecting the addresses?
-
+    //The two local int variables are stored next to each other in stack memory.Their addresses differ by 4 bytes, which matches sizeof(int) on this platform.
     return 0;
 }
