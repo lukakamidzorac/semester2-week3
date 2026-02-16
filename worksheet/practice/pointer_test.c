8bb5230c78a47b16941584c45499a7d9524c1a5c
lukakamidzorac <sjvj0907@leeds.ac.uk>
2026-02-16 13:38:35 +0000

    end of pointer

--- worksheet/practice/pointer_test.c
+++ worksheet/practice/pointer_test.c
@@ -1,25 +1,26 @@
-
 #include <stdio.h>
 
 /*
- * Exaple using pointers and dereferencing
+ * Example using pointers and dereferencing
  */
 
-int main( void ) {
+int main(void) {
 
-    float f = 2.3;
+    float f = 2.3f;
     int k = -2;
 
-    float *pf; // assign the pointer so that it can access the value of f
-    float *pk; // assign the pointer so taht is can access the value of k
+    float *pf = &f;  // pointer na f
+    int *pk = &k;    // pointer na k (mora biti int*)
 
-    sum = 0.0; // compute the sum of the primitive values with appropriate casting
+    float sum = 0.0f; // compute the sum of the primitive values with appropriate casting
+    sum = f + (float)k;
 
-    printf("%f\n", sum );
+    printf("%f\n", sum);
 
-    sum = 0.0; // compute the sum via the pointers, using appropriate casting
-    
-    printf("%f\n", sum );  // implement this using the pointers to print the same value
+    sum = 0.0f; // compute the sum via the pointers, using appropriate casting
+    sum = *pf + (float)(*pk);
+
+    printf("%f\n", sum); // same value, now using pointers
 
     return 0;
 }
